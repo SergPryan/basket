24f2535d02881522db1e4cfceb2a47d0e825ccfa
pryanichnikovsm <pryanichnikovsm@novotelecom.ru>
2020-05-24 15:35:39 +0700

    fix component scan

--- src/main/java/com/example/commons/BasketApplication.java
+++ src/main/java/com/example/commons/BasketApplication.java
@@ -2,9 +2,13 @@ package com.example.commons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@EntityScan("com.example.commons")
+@ComponentScan("com.example")
 public class BasketApplication {
 
 	public static void main(String[] args) {
